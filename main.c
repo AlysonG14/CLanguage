8a2f4b825712e7e5d081a89c050cd20797c40149
ATL8CA <atl8ca@bosch.com>
2026-04-28 16:53:46 -0300

    Update File 28/04 - feat: The 2° degree square root has finished

--- main.c
+++ main.c
@@ -1,40 +1,71 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main() {
+    int grau, continuar = 1;
 
-    int command = 0;
-    while(command <= 10){
-        printf("Number: %f",command);
-        command++;
-    }
+    while (continuar == 1) {
+        printf("\nEnter the degree of polynomial (0, 1, 2): ");
+        scanf("%d", &grau);
 
-    int command1 = 0, grau = 2;
-    float list[2], func;
-    float list1[3], func1;
-    while (command1 == 0){
-        printf("Type the values of coefficients of equations of 2° degree of sequence ");
-        for(int i = 0; i <= grau; i++){
-            printf("Type the coefficient of degree %d\n ",i);
-            scanf("%f\n", &list[i]);
-            
+        if (grau < 0 || grau > 2) {
+            printf("Invalid degree! Try again.\n");
+            continue;
         }
-        for(float i = -100; i <= 100; i+= 0.01){
-            func = list[2] * pow(i, 2) + list[1] * pow(i, 1) + list[0] * pow(i, 0);
-            func1 = list1[3] * pow(i, 3) + list1[2] * pow(i, 2) + list1[1] * pow(i, 1) + list1[0] * pow(i, 0);
-            if(func >= 0 && func < 0.1){
-                printf("A root can %f \n",i);
-            }
-            else if(func1 >= 0 && func1 < 0.1){
-                printf("A root can %f \n", i);
+
+        if (grau == 0) {
+            float c;
+            printf("Enter constant (c): ");
+            scanf("%f", &c);
+
+            if (c == 0) {
+                printf("Infinite solutions\n");
+            } else {
+                printf("No solution\n");
             }
         }
-        printf("Do you want to continue? Yes (0) - No (1)");
-        scanf("%d\n", &command1);
+
+        else if (grau == 1) {
+            float a, b;
+            printf("Enter a and b (ax + b = 0): ");
+            scanf("%f %f", &a, &b);
+
+            if (a == 0) {
+                printf("Invalid equation (a cannot be 0)\n");
+            } else {
+                float x = -b / a;
+                printf("Root: %.2f\n", x);
+            }
+        }
+
+        else {
+            float a, b, c;
+            printf("Enter a, b, c (ax^2 + bx + c = 0): ");
+            scanf("%f %f %f", &a, &b, &c);
+
+            if (a == 0) {
+                printf("This is not degree 2. Try again.\n");
+                continue;
+            }
+
+            float delta = b*b - 4*a*c;
+
+            if (delta < 0) {
+                printf("No real roots\n");
+            } else if (delta == 0) {
+                float x = -b / (2*a);
+                printf("One root: %.2f\n", x);
+            } else {
+                float x1 = (-b + sqrt(delta)) / (2*a);
+                float x2 = (-b - sqrt(delta)) / (2*a);
+                printf("Roots: %.2f and %.2f\n", x1, x2);
+            }
+        }
+
+        printf("\nDo you want to solve another? (1 = yes, 0 = no): ");
+        scanf("%d", &continuar);
     }
 
-    printf("dlrow olleH");
-
-
+    printf("\nProgram finished.\n");
     return 0;
 }
