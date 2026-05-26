74654499412f9f7d5db6decf056ba6b516f0fd35
ATL8CA <atl8ca@bosch.com>
2026-05-26 17:13:42 -0300

    Update File 26/05 - feat: 'Nothing special, only detX, detY, detX has been added'

--- 2_Matriz/matriz.c
+++ 2_Matriz/matriz.c
@@ -2,33 +2,32 @@
 #include <math.h>
 
 int main(void){
+    
+    // Matriz Method
 
+    int matriz[3][3], i, j;
 
-    // ********** Matriz Method ***********
-  
-  int matriz[3][3], i, j;
-  
-  printf("\nDigite os valores do elementos: \n \n");
-  
-  for(i = 0; i < 3; i++){ // i -> é o indíce da linha da matriz
-    for(j = 0; j < 3; j++){ // j -> é o indíce da coluna da matriz
-      
-        printf("\nElemento[%d][%d] = ", i, j);
-        scanf("%d", &matriz[i][j]);
+    printf("\n Digite os números do elementos: \n \n");
+
+    for(i = 0; i < 3; i++){ // i -> é o indíce da linha da matriz
+        for(j = 0; j < 3; j++){ // j -> é o indíce da coluna da matriz
+            printf("\n Elementos[%d][%d]", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
     }
-  }
-  
-  printf("\n\n******** Saída de Dados *********\n\n");
-  
-  for(i = 0; i < 3; i++){ // i -> é o indíce da linha da matriz
-    for(j = 0; j < 3; j++){ // j -> é o indíce da coluna da matriz
-      printf("\nElemento[%d][%d] = %d", i, j, matriz[i][j]);
-    }
-  }
 
-  // ********** Kramer Method ***********
-  
-  
-  
-  return 0;
+    printf("\n********Saída de Dados********\n");
+
+    for(i = 0; i < 3; i++){ // i -> é o indíce da linha da matriz
+        for(j = 0; j < 3; j++){ // j -> é o indíce da coluna da matriz
+            printf("\nElementos[%d][%d] = %d", i, j, matriz[i][j]);
+        }
+    }
+
+    // Kramer Method
+
+    int detX, detY, detX;
+
+
+    return 0;
 }
